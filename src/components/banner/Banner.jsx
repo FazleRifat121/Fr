6d47b980bb76ec2c48d04eb33688d0a10efaed2b
FazleRifat121 <fazlerifatofficial@gmail.com>
2024-10-30 02:39:20 +0600

    try to fix size problem

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
         <img src={img} className="max-w-sm md:max-w-xl rounded-lg " />
         <div>
           <h1 className="text-5xl font-bold">Hi, I am Fazle Rifat</h1>
-          <p className="py-6 container">
+          <p className="py-6 ">
             Frontend Web Developer Crafting seamless and engaging web
             experiences with a passion for design and innovation. Turning ideas
             into interactive realities through clean code and creativity.
--- src/components/mainLayout/MainLayout.jsx
+++ src/components/mainLayout/MainLayout.jsx
@@ -5,7 +5,7 @@ import Banner from "../banner/Banner";
 const MainLayout = () => {
   return (
     <div className="overflow-x-hidden antialiased text-neutral-300 selection:bg-cyan-300 selection:text-cyan-900">
-      <div className="fixed top-0 -z-10 w-screen h-screen"></div>
+      <div className="fixed top-0 -z-10 w-full h-full"></div>
       <div className=" absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]  "></div>
       <div className="container mx-auto px-8 my-5">
         <Navbar></Navbar>
